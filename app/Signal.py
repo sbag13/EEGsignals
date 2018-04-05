a73569115c7fbc2f3c6e5acf7dd666b707947d95
szymon_baginski <baginski.szymon@gmail.com>
2018-04-06 00:44:32 +0200

    calculate mean energy of features

--- app/Signal.py
+++ app/Signal.py
@@ -1,11 +1,11 @@
-from numpy import asarray
-from pywt import wavedec
+import numpy as np
+import pywt
 
 class Signal():
     def __init__(self, label, samples, frequency, attr=None):
         # can add personal info
         self.label = label
-        self.data = asarray(samples)
+        self.data = np.asarray(samples)
         self.frequency = frequency
         if attr is None:
             self.attr = {}
@@ -23,9 +23,21 @@ class Signal():
 
 class Epoch():
     def __init__(self, data):
-        self.data = asarray(data)
+        self.data = np.asarray(data)
     
     def extractFeatures(self):
-        self.coeffs = wavedec(self.data, 'db1, level=7)
-        for a in self.coeffs:
-            print(len(a))
+        self.wp = pywt.WaveletPacket(data=self.data, wavelet='db1', mode='symmetric')
+        features = {}
+        features['deltaEnergy'] = self.getEnergy(['aaaaaad', 'aaaaad', 'aaaada', 'aaaadd'])
+        features['thetaEnergy'] = self.getEnergy(['aaadaa', 'aaadad', 'aaadd', 'aadaa', 'aadada'])
+        features['alphaEnergy'] = self.getEnergy(['aadadd', 'aadda'])
+        features['spindleEnergy'] = self.getEnergy(['aaddd', 'adaaa', 'adaad'])
+        features['beta1Energy'] = self.getEnergy(['adad', 'adda'])
+        features['beta2Energy'] = self.getEnergy(['addd', 'da'])
+        print(features)
+    
+    def getEnergy(self, keys):
+        return np.mean([self.energy(self.wp[key].data) for key in keys])
+
+    def energy(self, data):
+        return np.sqrt(np.sum([x**2 for x in data]) / len(data))
