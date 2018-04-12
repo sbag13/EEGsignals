4f74a4204b3656dea738f53b905811a878649f6d
sbag13 <baginski.szymon@gmail.com>
2018-04-12 12:16:41 +0200

    calculated all features

--- app/Signal.py
+++ app/Signal.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pywt
+from statistics import stdev 
 
 class Signal():
     def __init__(self, label, samples, frequency, attr=None):
@@ -27,15 +28,49 @@ class Epoch():
     
     def extractFeatures(self):
         self.wp = pywt.WaveletPacket(data=self.data, wavelet='db1', mode='symmetric')
-        features = {}
-        features['deltaEnergy'] = self.getEnergy(['aaaaaad', 'aaaaad', 'aaaada', 'aaaadd'])
-        features['thetaEnergy'] = self.getEnergy(['aaadaa', 'aaadad', 'aaadd', 'aadaa', 'aadada'])
-        features['alphaEnergy'] = self.getEnergy(['aadadd', 'aadda'])
-        features['spindleEnergy'] = self.getEnergy(['aaddd', 'adaaa', 'adaad'])
-        features['beta1Energy'] = self.getEnergy(['adad', 'adda'])
-        features['beta2Energy'] = self.getEnergy(['addd', 'da'])
-        print(features)
-    
+        self.features = {}
+        self.features['deltaEnergy'] = self.getEnergy(['aaaaaad', 'aaaaad', 'aaaada', 'aaaadd'])
+        self.features['thetaEnergy'] = self.getEnergy(['aaadaa', 'aaadad', 'aaadd', 'aadaa', 'aadada'])
+        self.features['alphaEnergy'] = self.getEnergy(['aadadd', 'aadda'])
+        self.features['spindleEnergy'] = self.getEnergy(['aaddd', 'adaaa', 'adaad'])
+        self.features['beta1Energy'] = self.getEnergy(['adad', 'adda'])
+        self.features['beta2Energy'] = self.getEnergy(['addd', 'da'])
+        totalEnergy = sum(self.features.values())
+        self.features['totalEnergy'] = totalEnergy
+        self.features['alpha2DeltaPlusTheta'] = \
+            self.features['alphaEnergy'] / (self.features['deltaEnergy'] + self.features['thetaEnergy'])
+        self.features['delta2AlphaPlusTheta'] = \
+            self.features['deltaEnergy'] / (self.features['alphaEnergy'] + self.features['thetaEnergy'])
+        self.features['theta2DeltaPlusAlpha'] = \
+            self.features['thetaEnergy'] / (self.features['deltaEnergy'] + self.features['alphaEnergy'])
+        self.allCoeffs = self.getAllCoeffs()
+        self.features['allCoefficientsMean'] = np.mean(self.allCoeffs)
+        self.features['standardDevaition'] = stdev(self.allCoeffs)
+
+    def getAllCoeffs(self):
+        nodes = [self.wp.a.a.a.a.a.a.d, \
+            self.wp.a.a.a.a.a.d, \
+            self.wp.a.a.a.a.d.a, \
+            self.wp.a.a.a.a.d.d, \
+            self.wp.a.a.a.d.a.a, \
+            self.wp.a.a.a.d.a.d, \
+            self.wp.a.a.a.d.d, \
+            self.wp.a.a.d.a.a, \
+            self.wp.a.a.d.a.d.a, \
+            self.wp.a.a.d.a.d.d, \
+            self.wp.a.a.d.d.a, \
+            self.wp.a.a.d.d.d, \
+            self.wp.a.d.a.a.a, \
+            self.wp.a.d.a.a.d, \
+            self.wp.a.d.a.d, \
+            self.wp.a.d.d.a, \
+            self.wp.a.d.d.d, \
+            self.wp.d.a]
+        allCoeffs = []
+        for n in nodes:
+            allCoeffs.extend(n.data)
+        return allCoeffs    
+
     def getEnergy(self, keys):
         return np.mean([self.energy(self.wp[key].data) for key in keys])
 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from EdfFile import EdfFile
+import time
 
 edf_file = EdfFile("../SC4001E0-PSG.edf")
-e = edf_file.signals_list[1].getEpochs()
-e[0].extractFeatures()
-# print(edf_file.annotations)
-    
-# for sig in edf_file.signals_list:
-#     print(sig.label)
+epochs = edf_file.signals_list[1].getEpochs()
+# epochs[0].extractFeatures()
+start = time.time()
+for e in epochs:
+    e.extractFeatures()
+stop = time.time()
+print(stop - start)
